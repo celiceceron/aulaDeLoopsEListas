9a479640049da971c26ec978c959fab2a37b7e57
celiceceron <celice.cer@gmail.com>
2022-10-19 21:52:52 +0200

    codando com while

---
+++ ex-1.js
@@ -0,0 +1,15 @@
+/* 
+Crie uma função para exibir números de 1 a 10.
+Quando chamada a função mostra no console números de 1 a 10.
+*/
+
+function exibeNumerosDe1a10 (){
+    let i = 0;
+
+    while (i <= 10) {
+        console.log(i);
+        i = i + 1;
+       
+    }
+}
+exibeNumerosDe1a10();
